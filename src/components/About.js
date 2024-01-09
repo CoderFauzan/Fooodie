fb77897751d921468af68879d6b02e5b4c992bcf
coderfauzan <mahzaibfauzan@gmail.com>
2024-01-09 17:56:14 +0530

    class component chap8

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,30 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is about page</h2>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+
+
+  render() {
+    console.log("Parent render");
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is about page</h2>
+        <User name={"fauzan"} />
+        <UserClass name={"class"} location={"Delhi class"} />
+      </div>
+    );
+  }
+}
 
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInfo: {
+        name: "Dummy",
+        location: "Default",
+      },
+    };
+
+    console.log(props);
+  }
+
+  async componentDidMount() {
+    console.log(this.props.name + "Child Component Did Mount");
+    // Api call
+
+    const data = await fetch("https://api.github.com/users/coderfauzan");
+    const json = await data.json();
+
+    this.setState({
+      userInfo: json,
+    });
+
+    console.log(json);
+  }
+
+  componentDidUpdate() {
+    console.log("Child Component Did Update");
+  }
+
+  componentWillUnmount() {
+    console.log("Child Component Will Unmount");
+  }
+
+  render() {
+    const { name, location,avatar_url } = this.state.userInfo;
+    console.log("child render");
+    return (
+      <div className="user-card">
+        <img src={avatar_url}/>
+        <h2>Name:{name}</h2>
+        <h3>Location: {location}</h3>
+        <h4>Contact: @coderfauzan</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
